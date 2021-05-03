bdf7c01d2620eba78bddfec54c7540949c6972c7
Ta Minh Tri <tmtri92.td@gmail.com>
2021-05-03 11:24:39 +0700

    Update

--- redux/store/index.tsx
+++ redux/store/index.tsx
@@ -17,7 +17,7 @@ export const makeStore = (context) => {
   const sagaMiddleware = createSagaMiddleware();
   const store = createStore(rootReducer, bindMiddleware([sagaMiddleware]));
 
-  (store as any).sagaTask = sagaMiddleware.run(rootSaga);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
 
   return store;
 };
