988b7674ed22d7a6614d13561a7f75a33542bd05
Ta Minh Tri <tmtri92.td@gmail.com>
2021-05-03 15:01:06 +0700

    Update

--- pages/admin/email-invitation.tsx
+++ pages/admin/email-invitation.tsx
@@ -1,14 +1,14 @@
 export default function EmailInvitation() {
   return (
     <>
-      <h2>You have been invited 345345345345</h2>
+      <h2>You have been invited</h2>
 
       <p>
         You have been invited to create a user on {"{{ .SiteURL }}"}. Follow
         this link to accept the invite:
       </p>
       <p>
-        <a href="{{ .ConfirmationURL }}">Accept the invite</a>
+        <a href="{{ .ConfirmationURL }}/admin/">Accept the invite</a>
       </p>
     </>
   );
